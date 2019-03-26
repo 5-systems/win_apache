11f8bcad711a316ae5021a6c40e3bc98b94bcbb7
a.titov <a.titov@localhost.localdomain>
2019-03-26 09:09:35 +0300

    Изменен формат окончания строки для работы в win

--- getcallerdata.php
+++ getcallerdata.php
@@ -1,1 +1,42 @@
-<?php	header('Content-Type: text/html; charset=utf-8');		@$CallId=$_REQUEST['CallId'];	@$CallerNumber=$_REQUEST['CallerNumber'];		@$ParsedCallerNumber=$_REQUEST['ParsedCallerNumber'];		ini_set('default_socket_timeout', 10);	ini_set("soap.wsdl_cache_enabled", "0");	$SoapClient1C = new SoapClient("http://127.0.0.1:8080/database_name/ws/wsoktellexchange.1cws?wsdl", array('login'=>'WebService', 'password'=>'passwd'));	If(isset($CallerNumber)) {				$params=Array();		$params['CallId']=$CallId;		$params['CallerNumber']=$CallerNumber;		$params['ParsedCallerNumber']=$ParsedCallerNumber;		try{			$Result = $SoapClient1C->GetCallerData($params);		}		catch(Exception $e){			echo $e->getMessage();		}		if( gettype($Result)==="object" ) {			echo $Result->return;		}	}?>
+<?php
+
+	header('Content-Type: text/html; charset=utf-8');	
+
+	@$CallId=$_REQUEST['CallId'];
+	@$CallerNumber=$_REQUEST['CallerNumber'];	
+	@$ParsedCallerNumber=$_REQUEST['ParsedCallerNumber'];
+	
+	ini_set('default_socket_timeout', 10);
+
+	ini_set("soap.wsdl_cache_enabled", "0");
+
+	$SoapClient1C = new SoapClient("http://127.0.0.1:8080/database_name/ws/wsoktellexchange.1cws?wsdl", array('login'=>'WebService', 'password'=>'passwd'));
+
+
+	If(isset($CallerNumber)) {
+		
+		$params=Array();
+		$params['CallId']=$CallId;
+		$params['CallerNumber']=$CallerNumber;
+		$params['ParsedCallerNumber']=$ParsedCallerNumber;
+
+		try{
+			$Result = $SoapClient1C->GetCallerData($params);
+		}
+		catch(Exception $e){
+			echo $e->getMessage();
+		}
+
+
+		if( gettype($Result)==="object" ) {
+
+			echo $Result->return;
+
+		}
+
+
+	}
+
+
+?>
+
--- getcalltype.php
+++ getcalltype.php
@@ -1,1 +1,38 @@
-<?php	header('Content-Type: text/html; charset=utf-8');		@$CallerNumber=$_REQUEST['CallerNumber'];			ini_set('default_socket_timeout', 10);	ini_set("soap.wsdl_cache_enabled", "0");	$SoapClient1C = new SoapClient("http://127.0.0.1:8080/database_name/ws/wsoktellexchange.1cws?wsdl", array('login'=>'WebService', 'password'=>'passwd'));	If(isset($CallerNumber)) {		$params=Array();		$params['CallerNumber']=$CallerNumber;				try{			$Result = $SoapClient1C->GetCallType($params);		}		catch(Exception $e) {			echo $e->getMessage();		}		if( gettype($Result)==="object" ) {			echo $Result->return;		}	}?>
+<?php
+
+	header('Content-Type: text/html; charset=utf-8');	
+
+
+	@$CallerNumber=$_REQUEST['CallerNumber'];	
+	
+	ini_set('default_socket_timeout', 10);
+
+	ini_set("soap.wsdl_cache_enabled", "0");
+
+	$SoapClient1C = new SoapClient("http://127.0.0.1:8080/database_name/ws/wsoktellexchange.1cws?wsdl", array('login'=>'WebService', 'password'=>'passwd'));
+
+
+	If(isset($CallerNumber)) {
+		$params=Array();
+		$params['CallerNumber']=$CallerNumber;
+		
+		try{
+
+			$Result = $SoapClient1C->GetCallType($params);
+		}
+		catch(Exception $e) {
+			echo $e->getMessage();
+		}
+
+
+		if( gettype($Result)==="object" ) {
+
+			echo $Result->return;
+
+		}
+
+	}
+
+
+?>
+
